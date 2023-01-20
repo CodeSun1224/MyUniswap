fd5cdfcf1a13cd1eb07215aa9f75538ed05652e8
codesun <codesun1224@gmail.com>
2023-01-20 22:42:16 +0800

    测试添加流动性功能

--- contract/test/V2PairTest.sol
+++ contract/test/V2PairTest.sol
@@ -27,17 +27,45 @@ contract V2PairTest is Test{
         V2PairFactory factory = new V2PairFactory();
         address pairAddress = factory.createPair(address(token0), address(token1));
         pair = V2Pair(pairAddress);
+        token0.mint(address(this), 10 ether);
+        token1.mint(address(this), 10 ether);
+    }
+
+    function testTokenAddress() public view {
+        // createPair要对token地址的大小排序
+        assert(address(token0) > address(token1));
     }
 
     function testMint() public {
-        token0.mint(address(this), 1 ether);
-        token1.mint(address(this), 1 ether);
-
+        // 首次添加
         token0.transfer(address(pair), 1 ether);
         token1.transfer(address(pair), 1 ether);
 
         pair.mint(address(this));
         assertEq(pair.balanceOf(address(this)), 1 ether - 1000);
         assertEq(pair.totalSupply(), 1 ether);
+        assertReserves(1 ether, 1 ether);
+
+        // 按比例添加
+        token0.transfer(address(pair), 1 ether);
+        token1.transfer(address(pair), 1 ether);
+        pair.mint(address(this));
+        assertEq(pair.balanceOf(address(this)), 2 ether - 1000); // 1 ether - 1000 + 1 ether
+        assertEq(pair.totalSupply(), 2 ether); // 1 ether + 1 ether
+        assertReserves(2 ether, 2 ether); // 1 + 1 ， 1 + 1
+
+        // 不按比例添加，取最小的，算出的流动性还是1 ether
+        token0.transfer(address(pair), 2 ether);
+        token1.transfer(address(pair), 1 ether);
+        pair.mint(address(this));
+        assertEq(pair.balanceOf(address(this)), 3 ether - 1000); // 1 ether - 1000 + 1 ether + 1 ether
+        assertEq(pair.totalSupply(), 3 ether); // 1 + 1 + 1
+        assertReserves(3 ether, 4 ether); // 2 + 2 ， 2 + 1
+    }
+
+    function assertReserves(uint112 reserve0_, uint112 reserve1_) internal {
+        (uint112 reserve0, uint112 reserve1, ) = pair.getReserves();
+        assertEq(reserve0, reserve0_);
+        assertEq(reserve1, reserve1_);
     }
 }
